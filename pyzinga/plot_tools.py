69943d1fed7fe429a715955ec9a054942095a874
Shaz <shatadrusaha@gmail.com>
2025-07-27 20:21:46 +0100

    updating the plot_tools.py script

--- pyzinga/plot_tools.py
+++ pyzinga/plot_tools.py
@@ -1,159 +1,322 @@
-"""                     Import libraries.                       """
-import pandas as pd  # noqa: E402
-import seaborn as sns  # noqa: E402
-import matplotlib.pyplot as plt  # noqa: E402
-import numpy as np  # noqa: E402
-from typing import Tuple, Optional  # noqa: E402
-import os  # noqa: E402
+"""Import libraries."""
+import os
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+from typing import Tuple, Optional
 
-# Function for plotting correlation matrix.
-def plot_correlation_matrix(
-        df: pd.DataFrame, 
-        figsize: Tuple[float, float] = (10, 8),
-        col_target: Optional[str] = None,
-        folder_tosave_plot: Optional[str] = None,
-        cmap: str = 'coolwarm',
-        annot_fmt: str = ".2f",
-        show_both_masks: bool = True
-    ) -> pd.DataFrame:
+
+"""                     Define settings for plotting.                       """
+# Default plotting parameters for plots.
+DEFAULT_FIGSIZE = (10, 8)
+DEFAULT_HUE = None
+DEFAULT_EDGECOLOR = "black"
+DEFAULT_LEGEND = False
+DEFAULT_STYLE = "darkgrid"
+DEFAULT_PALETTE = "deep"
+DEFAULT_SAVEDIR = None
+DEFAULT_CMAP = "coolwarm"
+
+sns.set_theme(style=DEFAULT_STYLE, palette=DEFAULT_PALETTE)
+
+
+"""                     Functions.                       """
+# Function for count plot.
+def count_plot(
+    df: pd.DataFrame,
+    col: str,
+    figsize: Tuple[int, int] = DEFAULT_FIGSIZE,
+    save_dir: Optional[str] = DEFAULT_SAVEDIR,
+    **kwargs,
+) -> None:
     """
-    Plot correlation matrix or correlation with respect to a target column.
-    
+    Create a count plot for a categorical column.
+
     Parameters
     ----------
     df : pd.DataFrame
-        Input DataFrame containing the features to calculate correlation for.
-    figsize : tuple of (float, float), default=(10, 8)
-        Figure size in inches as a tuple of (width, height).
-    col_target : str, optional
-        If provided, plot correlation of all features with respect to this target column.
-        Must be a numeric column in the DataFrame.
-    folder_tosave_plot : str, optional
-        If provided, save the figure to this folder. The folder will be created if it doesn't exist.
-    cmap : str, default='coolwarm'
-        Colormap name to use for the heatmap.
-    annot_fmt : str, default='.2f'
-        String formatting code for the annotations.
-    show_both_masks : bool, default=True
-        If True, show both masked and unmasked correlation matrices when col_target is None.
-        If False, only show the unmasked version.
-    
+        Input DataFrame.
+    col : str
+        Column name to plot.
+    figsize : tuple of int, optional
+        Figure size, by default (10, 8).
+    **kwargs
+        Additional keyword arguments passed to seaborn.countplot.
+
     Returns
     -------
-    pd.DataFrame
-        The correlation DataFrame.
-    
+    None
+    """
+    # Define plot title and labels.
+    title = f"Count (frequency) plot of '{col.replace('_', ' ').title()}'"
+    xlabel = f"{col.replace('_', ' ').title()}"
+    ylabel = "Count"
+
+    # Fill missing values in the column with 'X' and convert to category type.
+    df = df.copy()
+    if df[col].isnull().sum() > 0:
+        df[col] = df[col].fillna("X").astype("category")
+
+    # Set defaults if not provided in kwargs
+    kwargs.setdefault("palette", DEFAULT_PALETTE)
+    kwargs.setdefault("hue", DEFAULT_HUE)
+    kwargs.setdefault("edgecolor", DEFAULT_EDGECOLOR)
+    kwargs.setdefault("legend", DEFAULT_LEGEND)
+    kwargs.setdefault("stat", "count")
+
+    fig, ax = plt.subplots(figsize=figsize)
+    sns.countplot(data=df, x=col, ax=ax, **kwargs)
+    ax.set_title(label=title)
+    ax.set_xlabel(xlabel=xlabel)
+    ax.set_ylabel(ylabel=ylabel)
+
+    # Optionally, handle legend for seaborn < 0.12
+    # ax.legend().set_visible(False)
+
+    # Save the plot if a directory is specified.
+    if save_dir is not None:
+        plt.savefig(
+            fname=os.path.join(save_dir, f"count_plot_{col.lower()}.png"),
+            dpi=300,
+            bbox_inches="tight",
+        )
+        print(
+            f"Count plot saved at '{os.path.join(save_dir, f'count_plot_{col.lower()}.png')}'"
+        )
+    plt.show()
+    plt.close(fig)
+
+
+# Function for histogram plot.
+def histogram_plot(
+    df: pd.DataFrame,
+    col: str,
+    figsize: Tuple[int, int] = DEFAULT_FIGSIZE,
+    save_dir: Optional[str] = DEFAULT_SAVEDIR,
+    prefix: Optional[str] = None,
+    **kwargs,
+) -> None:
+    """
+    Create a histogram plot for a numerical column.
+
+    Parameters
+    ----------
+    df : pd.DataFrame
+        Input DataFrame.
+    col : str
+        Column name to plot.
+    figsize : tuple of int, optional
+        Figure size, by default (10, 8).
+    prefix : str, optional
+        Prefix for the plot title and filename.
+    **kwargs
+        Additional keyword arguments passed to seaborn.histplot.
+
+    Returns
+    -------
+    None
+    """
+    # Define plot title, filename and labels.
+    title = f"Histogram Plot of '{col.replace('_', ' ').title()}'"
+    filename = f"histogram_plot_{col.replace(' ', '_').lower()}.png"
+    if prefix is not None:
+        title = f"{prefix} {title}"
+        filename = f"{prefix.lower()}_{filename}"
+    xlabel = f"{col.replace('_', ' ').title()}"
+    ylabel = "Frequency"
+
+    # Set defaults if not provided in kwargs
+    kwargs.setdefault("bins", "auto")
+    kwargs.setdefault("kde", True)
+    kwargs.setdefault("color", "teal")
+    kwargs.setdefault("edgecolor", DEFAULT_EDGECOLOR)
+    kwargs.setdefault("legend", DEFAULT_LEGEND)
+
+    # Plot the image.
+    fig, ax = plt.subplots(figsize=figsize)
+    sns.histplot(data=df, x=col, ax=ax, **kwargs)
+    ax.set_title(title)
+    ax.set_xlabel(xlabel)
+    ax.set_ylabel(ylabel)
+
+    # Save the plot if a directory is specified.
+    if save_dir is not None:
+        plt.savefig(
+            fname=os.path.join(save_dir, filename), dpi=300, bbox_inches="tight"
+        )
+        print(f"Histogram plot saved at '{os.path.join(save_dir, filename)}'")
+    plt.show()
+    plt.close(fig)
+
+
+# Function for box plot.
+def box_plot(
+    df: pd.DataFrame,
+    x_col: Optional[str] = None,
+    y_col: Optional[str] = None,
+    yscale: Optional[str] = None,
+    figsize: Tuple[int, int] = DEFAULT_FIGSIZE,
+    save_dir: Optional[str] = DEFAULT_SAVEDIR,
+    prefix: Optional[str] = None,
+    **kwargs,
+) -> None:
+    """
+    Create a box plot for one or two columns.
+
+    Parameters
+    ----------
+    df : pd.DataFrame
+        Input DataFrame.
+    x_col : str, optional
+        Column name for x-axis (categorical).
+    y_col : str, optional
+        Column name for y-axis (numerical).
+    yscale : str, optional
+        Scale for y-axis ('log' for log scale).
+    figsize : tuple of int, optional
+        Figure size, by default (10, 8).
+    prefix : str, optional
+        Prefix for the plot title and filename.
+    **kwargs
+        Additional keyword arguments passed to seaborn.boxplot.
+
+    Returns
+    -------
+    None
+
     Raises
     ------
     ValueError
-        If the DataFrame is empty, target column is not found, or target column
-        is not numeric.
-    TypeError
-        If inputs are not of the correct types.
-    
-    Examples
-    --------
-    >>> import pandas as pd
-    >>> import numpy as np
-    >>> df = pd.DataFrame({
-    ...     'A': np.random.rand(100),
-    ...     'B': np.random.rand(100),
-    ...     'C': np.random.rand(100)
-    ... })
-    >>> corr = plot_correlation_matrix(df)
-    >>> corr = plot_correlation_matrix(df, col_target='A')
+        If both x_col and y_col are None.
     """
-    # Input validation
-    if not isinstance(df, pd.DataFrame):
-        raise TypeError("df must be a pandas DataFrame")
-        
-    if df.empty:
-        raise ValueError("The DataFrame is empty. Cannot compute correlation matrix.")
-        
-    if not isinstance(figsize, tuple) or len(figsize) != 2:
-        raise TypeError("figsize must be a tuple of two numbers")
-        
-    if not all(isinstance(x, (int, float)) and x > 0 for x in figsize):
-        raise ValueError("figsize values must be positive numbers")
-        
-    if folder_tosave_plot is not None and not isinstance(folder_tosave_plot, str):
-        raise TypeError("folder_tosave_plot must be None or a string")
-        
-    if not isinstance(cmap, str):
-        raise TypeError("cmap must be a string")
+    # Validate input columns.
+    if x_col is None and y_col is None:
+        raise ValueError("At least one of 'x_col' or 'y_col' must be provided.")
 
-    # Set the default font size based on the figure size.
-    fontsize = figsize[0] * 1.5
+    # Define plot title and labels.
+    if x_col is not None and y_col is not None:
+        title = f"Box Plot of '{y_col.replace('_', ' ').title()}' w.r.t. '{x_col.replace('_', ' ').title()}'"
+        if prefix is not None:
+            title = f"{prefix} {title}"
 
-    # Create folder if it doesn't exist
-    if folder_tosave_plot is not None:
-        if not os.path.exists(folder_tosave_plot):
-            os.makedirs(folder_tosave_plot)
-            print(f"Created directory '{folder_tosave_plot}' for saving plots.")
+        xlabel = f"{x_col.replace('_', ' ').title()}"
+        ylabel = (
+            f"{y_col.replace('_', ' ').title()}"
+            if yscale is None
+            else f"{y_col.replace('_', ' ').title()} (log scale)"
+        )
 
-    if col_target is not None:
-        if col_target not in df.columns:
-            raise ValueError(f"Target column '{col_target}' not found in the DataFrame.")
-        if df[col_target].dtype not in [np.float64, np.int64]:
-            raise ValueError(f"Target column '{col_target}' must be numeric (float or int).")
-        
-        # Calculate the correlation with respect to the target column.
-        corr = df.corr()[col_target].drop(labels=col_target, axis=0).sort_values(ascending=False)
+        filename = f"box_plot_{x_col.lower()}_vs_{y_col.lower()}.png"
+    elif x_col is not None:
+        title = f"Box Plot of '{x_col.replace('_', ' ').title()}'"
+        if prefix is not None:
+            title = f"{prefix} {title}"
 
-        # Plot correlation with respect to the target column.
-        fig, ax = plt.subplots(figsize=figsize)
-        sns.barplot(x=corr.values, y=corr.index, hue=corr.index, palette=cmap, legend=False, ax=ax)
-        ax.set_title(f"Correlation Plot w.r.t. Target Column ('{col_target}')", fontsize=fontsize, loc='center')
-        ax.set_xlabel('Correlation Coefficient')
-        ax.set_ylabel('Features')
-        plt.tight_layout()
-        
-        if folder_tosave_plot is not None:
-            filename_tosave = "correlation_plot_wrt_target.png"
-            plt.savefig(os.path.join(folder_tosave_plot, filename_tosave), 
-                        dpi=600, 
-                        bbox_inches='tight')
-        
-        plt.show()
-        plt.close(fig)
-
+        xlabel = f"{x_col.replace('_', ' ').title()}"
+        ylabel = None
+        filename = f"box_plot_{x_col.lower()}.png"
     else:
-        # Calculate the correlation matrix for the entire DataFrame.
-        corr = df.corr()
+        title = f"Box Plot of '{y_col.replace('_', ' ').title()}'"
+        if prefix is not None:
+            title = f"{prefix} {title}"
 
-        # Mask the upper triangle of the correlation matrix.
-        mask = np.triu(np.ones_like(corr, dtype=bool))
+        xlabel = None
+        ylabel = (
+            f"{y_col.replace('_', ' ').title()}"
+            if yscale is None
+            else f"{y_col.replace('_', ' ').title()} (log scale)"
+        )
+        filename = f"box_plot_{y_col.lower()}.png"
+    if prefix is not None:
+        filename = f"{prefix.lower()}_{filename}"
 
-        # Plot and save (if applicable) the correlation matrix.
-        plot_types = [mask, None] if show_both_masks else [None]
-        
-        for plot_type in plot_types:
-            fig, ax = plt.subplots(figsize=figsize)
-            sns.heatmap(
-                data=corr, 
-                annot=True, 
-                cmap=cmap, 
-                fmt=annot_fmt, 
-                mask=plot_type,
-                ax=ax
-            )
-            
-            title = "Correlation Matrix"
-            if plot_type is not None:
-                title += " (with mask)"
-                filename_tosave = "correlation_matrix_masked.png"
-            else:
-                filename_tosave = "correlation_matrix.png"
-            ax.set_title(title, fontsize=fontsize, loc='center')
-            
-            plt.tight_layout()
-            
-            if folder_tosave_plot is not None:
-                plt.savefig(os.path.join(folder_tosave_plot, filename_tosave), 
-                            dpi=600, # Save as PNG with high resolution.
-                            bbox_inches='tight') 
-            
-            plt.show()
-            plt.close(fig)
-    
-    return corr
+    # Normalize filename
+    filename = filename.replace(" ", "_")
+
+    # Set defaults if not provided in kwargs
+    kwargs.setdefault("palette", "Set2")
+    kwargs.setdefault("hue", x_col)
+
+    # Plot the image.
+    fig, ax = plt.subplots(figsize=figsize)
+    sns.boxplot(data=df, x=x_col, y=y_col, ax=ax, **kwargs)
+    if yscale == "log":
+        plt.yscale("log")
+    ax.set_title(title)
+    ax.set_xlabel(xlabel)
+    ax.set_ylabel(ylabel)
+
+    # Save the plot if a directory is specified.
+    if save_dir is not None:
+        plt.savefig(
+            fname=os.path.join(save_dir, filename), dpi=300, bbox_inches="tight"
+        )
+        print(f"Box plot saved at '{os.path.join(save_dir, filename)}'")
+    plt.show()
+    plt.close(fig)
+
+
+# Function for correlation heatmap.
+def corr_heatmap_plot(
+    df: pd.DataFrame,
+    save_dir: Optional[str] = DEFAULT_SAVEDIR,
+    figsize: Tuple[int, int] = DEFAULT_FIGSIZE,
+    mask_upper: bool = True,
+    **kwargs,
+) -> None:
+    """
+    Create a correlation heatmap for numeric columns.
+
+    Parameters
+    ----------
+    df : pd.DataFrame
+        Input DataFrame.
+    save_dir : str, optional
+        Directory to save the plot. If None, plot is not saved.
+    figsize : tuple of int, optional
+        Figure size, by default (10, 8).
+    mask_upper : bool, optional
+        If True, mask the upper triangle of the heatmap.
+    **kwargs
+        Additional keyword arguments passed to seaborn.heatmap.
+
+    Returns
+    -------
+    None
+    """
+    # Define plot title.
+    title = "Correlation Heatmap (masked)" if mask_upper else "Correlation Heatmap"
+    filename = (
+        "correlation_heatmap_masked.png" if mask_upper else "correlation_heatmap.png"
+    )
+
+    # Ensure the DataFrame contains only numeric columns for correlation.
+    df = df.select_dtypes(include=["number"])
+    corr = df.corr()  # Correlation matrix.
+    mask = np.triu(np.ones_like(corr, dtype=bool)) if mask_upper else None
+
+    # Set defaults if not provided in kwargs
+    kwargs.setdefault("cmap", DEFAULT_CMAP)
+    kwargs.setdefault("cbar_kws", {"shrink": 1.0})
+
+    # Plot the image.
+    fig, ax = plt.subplots(figsize=figsize)
+    sns.heatmap(
+        data=corr, annot=True, fmt=".2f", ax=ax, square=True, mask=mask, **kwargs
+    )
+    ax.set_title(title)
+    plt.xticks(rotation=45)  # Rotate x-axis labels for better readability
+    ax.set_aspect("equal")  # Ensure each cell is square and grid aligns
+    ax.grid(True, which="both", axis="both")  # Explicitly enable grid lines
+
+    # Save the plot if a directory is specified.
+    if save_dir is not None:
+        os.makedirs(name=save_dir, exist_ok=True)
+        plt.savefig(
+            fname=os.path.join(save_dir, filename), dpi=300, bbox_inches="tight"
+        )
+        print(f"Correlation heatmap saved at '{os.path.join(save_dir, filename)}'")
+
+    plt.show()
+    plt.close(fig)
